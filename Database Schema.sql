3215ba1a7ebaacc7565f3b02fc8a38fa848aa6b6
Rettus code <michael.rettusii@gmail.com>
2020-07-15 22:26:50 -0700

    initial commit

---
+++ Database Schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS data_db;
+CREATE DATABASE data_db;
+USE data_db;
+
+
+
+CREATE TABLE department(
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL(10, 2),
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30),
+  LAST_name VARCHAR(30),
+  ROLE_ID int, 
+  MANAGER_id INT,
+  PRIMARY KEY (id)
+);
---
+++ employee.js
@@ -0,0 +1,57 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "dataDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+  function start() {
+      inquirer.prompt({
+          type: "list",
+          name: "initial",
+          message: "Choose an action",
+          choices: ["Add departments, roles, employees", "View departments, roles, employees", "Update employee roles"]
+      })
+      .then(function(answer) {
+        switch (answer.initial) {
+        case "Add departments, roles, employees":
+          create();
+          break;
+  
+        case "View departments, roles, employees":
+          read();
+          break;
+  
+        case "Update employee roles":
+          update();
+          break;
+  
+        /*case "Update employee roles":
+          delete();
+          break;
+  
+        case "Find artists with a top song and top album in the same year":
+          songAndAlbumSearch();
+          break;
+        }*/
+      };
+    });
+  };
